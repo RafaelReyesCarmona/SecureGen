a06557db867eeb547ac990e3478f4105be100558
RafaelReyesCarmona <78477791+RafaelReyesCarmona@users.noreply.github.com>
2026-07-20 19:11:13 +0200

    Update doc

--- website/astro.config.mjs
+++ website/astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'astro/config';
 
 export default defineConfig({
   site: 'https://RafaelReyesCarmona.github.io',
-  base: '/SecureGen',
+  base: '/SecureGen-es_ES',
   output: 'static',
   build: {
     assets: 'assets',
